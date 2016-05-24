e0a4177b8a289d16612a183c534ab12f6aeed8c3
Liuyingao <53319042@qq.com>
2016-05-24 09:57:51 +0800

    change view controller.swift

--- RAP/ViewController.swift
+++ RAP/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello")
+        print("Hello 222")
     }
 
     override func didReceiveMemoryWarning() {
