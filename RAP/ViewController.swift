fc5e58a6b8b4027bfe7ce94efb8330c248f08d0e
Liuyingao <53319042@qq.com>
2016-05-24 13:24:09 +0800

    commit

--- RAP/ViewController.swift
+++ RAP/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello 222")
     }
 
     override func didReceiveMemoryWarning() {
