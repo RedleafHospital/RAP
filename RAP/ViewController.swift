4e04d8b371b7d61cf367e23b1c58fc68d41b8343
Liuyingao <53319042@qq.com>
2016-05-24 08:52:16 +0800

    Initial Commit

---
+++ RAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RAP
+//
+//  Created by 刘颖奥 on 16/5/24.
+//  Copyright © 2016年 Redleaf Hospital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
